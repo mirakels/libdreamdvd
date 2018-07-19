02a0e7cb0c37f98fbbaae087f7f5c010c5c7c9a3
Mirakels <mirakels@openpli.org>
2018-07-19 11:19:30 +0200

    Fix 16:9 handling
    Thanks ims for finding this in upstream dream

--- main.c
+++ main.c
@@ -1170,10 +1170,12 @@ send_message:
 							for (int z = 0; z < 2040; z++) {
 								if (buf[z] == 0x0 && buf[z + 1] == 0x0 && buf[z + 2] ==0x01 && buf[z + 3] == 0xB5 &&
 									(buf[z + 4] == 0x22 || buf[z + 4] == 0x23) ) {
-									buf[z + 5] = 0x0B;
-									buf[z + 6] = 0x42;
-									buf[z + 7] = 0x12;
-									buf[z + 8] = 0x00;
+									z += (buf[z + 4] & 0x01) ? 8 : 5;
+									buf[z]     = 0x0B;
+									buf[z + 1] = 0x42;
+									buf[z + 2] = 0x12;
+									buf[z + 3] = 0x00;
+									z += 3;
 								}
 							}
 							if (buf[33] == 0 && buf[33 + 1] == 0 && buf[33 + 2] == 1 && buf[33 + 3] == 0xB3)
